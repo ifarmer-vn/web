79fc6ad38911a601ea12bb3a1baa4396215ddf5c
haibui <hoanghaibui2003@gmail.com>
2019-07-16 03:47:33 +0800

    Play around with pug

--- frontend/views/pages/homepage/controller.js
+++ frontend/views/pages/homepage/controller.js
@@ -1,5 +1,10 @@
+const pug = require('pug');
+const {resolve} = require("path");
+const compiledFunction = pug.compileFile(resolve("views/pages/homepage/views/view.pug"));
 const controller = (req, res) => {
-	return res.send('Hello World!');
+	return res.send(compiledFunction({
+		name: 'Timothy'
+	}));
 };
 
 module.exports = controller;
