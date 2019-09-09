3a8dc643855ac8e78b4c97f00b6950da80b41d3d
Hai Bui <hoanghaibui2003@gmail.com>
2019-09-09 12:02:54 +0800

    Binding data: Top products

--- frontend/resources/pages/homepage/services/composeData.js
+++ frontend/resources/pages/homepage/services/composeData.js
@@ -1,11 +1,13 @@
 const css = require("../../../../src/css/css");
 const categories = require("./categoriesService");
+const variants = require("./variantsService");
 const R = require("ramda");
 let data = require("../data-feed/homepage");
 
 const getData = async () => {
 	let result = R.clone(data);
 	result.categories = await categories.getAllCategories();
+	result.topProducts = await variants.getTopProducts();
 	result.css = css.getFileContent("./assets/css/ifarmer-homepage-min.css");
 	return result;
 };
