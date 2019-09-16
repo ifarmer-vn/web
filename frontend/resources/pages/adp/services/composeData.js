5b6d20e880436d90ca147fb1c44730af0f7b090a
Hai Bui <hoanghaibui2003@gmail.com>
2019-09-17 00:11:56 +0800

    IF-133: structured data for ADP

--- frontend/resources/pages/adp/services/composeData.js
+++ frontend/resources/pages/adp/services/composeData.js
@@ -8,9 +8,66 @@ const getData = async (articleID) => {
     result.topArticles = await articles.getTopArticles();
     result.articleDetail = await articles.getArticle(articleID);
     result.topArticlesADP = await articles.getTopArticles(5);
+    result.structuredData = buildStructuredData(result.articleDetail);
     result.css = css.getFileContent("./assets/css/ifarmer-adp-min.css");
     return result;
 };
+
+const buildStructuredData = (article) => {
+    let result = [];
+    result.push(buildArticleStructuredData(article));
+    return result;
+};
+
+const buildArticleStructuredData = (article) => {
+    const articleDetail = article._source;
+    const url = `http://ifarmer.vn/bai-viet/${articleDetail.url}/`;
+    let result = {
+        "@context": "http://schema.org",
+        "@type": "Article",
+        headline: articleDetail.title,
+        url: url,
+        dateModified: articleDetail.updatedAt,
+        datePublished: articleDetail.createdAt,
+        description: articleDetail.description,
+        mainEntityOfPage: {
+            "@type": "WebPage",
+            "@id": url
+        },
+        author: {
+            "@type": "Person",
+            name: articleDetail.author,
+        },
+
+        publisher: {
+            "@type": "Organization",
+            name: "ifarmer.vn",
+            logo: {
+                "@type": "ImageObject",
+                url: "http://ifarmer.vn/assets/img/icons/favicon-96x96.png",
+                width: 57,
+                height: 57,
+                sameAs: [
+                    "https://www.youtube.com/channel/UCWpNAh53Cy11hy693mMdHlQ",
+                    "https://www.facebook.com/ifarmer.vn/"
+                ]
+            },
+        },
+    };
+    //Todo article must have hero image
+    if(articleDetail.images)
+    {
+        result.image = {
+            "@type": "ImageObject",
+            url: articleDetail.images[0].url,
+            width: 1200,
+            height: 675,
+
+        };
+    }
+    return result;
+};
+
 const revealed = {
     getData,
 };
--- frontend/resources/pages/adp/services/models/adpModel.js
+++ frontend/resources/pages/adp/services/models/adpModel.js
@@ -1,30 +1,33 @@
 let data = require("../../data-feed/adp");
 
 const adpModel = {
-	get title() {
-		return data.title;
-	},
-	get heading() {
-		return data.heading;
-	},
-	get css() {
-		return data.css;
-	},
-	get topArticles() {
-		return data.topArticles;
-	},
-	get articleDetail() {
-		return data.articleDetail;
-	},
-	get topArticlesADP() {
-		return data.topArticlesADP;
-	},
-	get canonical(){
-		return data.canonical;
-	},
-	set __data(d) {
-		data = d;
-	},
+    get title() {
+        return data.title;
+    },
+    get heading() {
+        return data.heading;
+    },
+    get css() {
+        return data.css;
+    },
+    get topArticles() {
+        return data.topArticles;
+    },
+    get articleDetail() {
+        return data.articleDetail;
+    },
+    get structuredData() {
+        return data.structuredData;
+    },
+    get topArticlesADP() {
+        return data.topArticlesADP;
+    },
+    get canonical() {
+        return data.canonical;
+    },
+    set __data(d) {
+        data = d;
+    },
 
 };
 module.exports = adpModel;
