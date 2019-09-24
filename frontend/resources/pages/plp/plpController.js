c93ff2fba978b6717be3c1f73ef4d853ec5c1f20
Hai Bui <hoanghaibui2003@gmail.com>
2019-09-25 01:04:00 +0800

    IF-170: Sitemap Robot 404

--- frontend/resources/pages/plp/plpController.js
+++ frontend/resources/pages/plp/plpController.js
@@ -7,6 +7,11 @@ let plpController = async (req, res) => {
     }
     console.time("Prepare data for plp");
     let data = await plpService.prepareData(categoryID);
+    if (!data.products.length) {
+        res.status(404);
+        res.type('txt').send('Not found');
+        return;
+    }
     console.timeEnd("Prepare data for plp");
     return res.render("pages/plp/views/plp", data);
 };
