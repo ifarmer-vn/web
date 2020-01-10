e8a475e1664a6871d0a85f1acc89fabf074efab9
Hai Bui <hoanghaibui2003@gmail.com>
2020-01-10 14:57:10 +0700

    Create redirect page

---
+++ frontend/resources/pages/redirect/services/composeData.js
@@ -0,0 +1,43 @@
+const css = require("../../../../src/css/css");
+const R = require("ramda");
+const searchProxy = require("../../../../src/elasticsearch/searchProxy");
+const variants = require("../../../../src/variants/variantsService");
+const products = require("../../../../src/products/productsService");
+let data = require("../data-feed/data");
+
+const getData = async (variantID) => {
+    let result = R.clone(data);
+
+    await getDataFromES(result, variantID);
+    result.productDetail = buildProductDetail(result.product, result.variant);
+    result.css = css.getFileContent("./assets/css/ifarmer-style-min.css");
+    result.title = result.variant._source.title;
+    result.canonical = `https://ifarmer.vn/chuyen-huong/${variantID}/`;
+    return result;
+};
+
+const getDataFromES = async (result, variantID) => {
+    let ship = searchProxy.createShip();
+    ship.addQuery("variants_v1", variants.getVariant(variantID));
+    let data = await ship.flush();
+    result.variant = data[0].hits.hits[0]; //for detail
+    const productUrl = result.variant._source.productSource.url;
+
+    ship.addQuery("products_v1", products.getProduct(productUrl));
+
+    data = await ship.flush();
+    result.product = data[0].hits.hits[0]; //for detail
+};
+
+const buildProductDetail = (product, variant) => {
+    let result = {
+        ...product._source,
+        ...variant._source,
+        h1: variant._source.title ? variant._source.title : `${product._source.title}  ${variant._source.extraTitle}`
+    };
+    return result;
+};
+const revealed = {
+    getData,
+};
+module.exports = revealed;
