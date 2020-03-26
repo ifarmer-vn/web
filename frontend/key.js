fef8606aa9ba10eb02d75ec0786f1ad707fee910
Hai Bui <hoanghaibui2003@gmail.com>
2020-03-26 23:29:38 +0700

    aa

--- frontend/key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    googleClientID: '349932054569-k8l925irmefu23d09cc63b5oe3qglvm1.apps.googleusercontent.com',
-    cookieKey: '123412',
-    googleClientSecret: 'tfCgMxJKXjWUOhjOmw8vQU7O'
-};
