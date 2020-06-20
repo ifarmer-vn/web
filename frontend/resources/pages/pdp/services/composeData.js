58f96bc86607f351c9536d2818cc951a1a404552
Hai Bui <hoanghaibui2003@gmail.com>
2020-06-20 23:29:36 +0700

    Enhance more

--- frontend/resources/pages/pdp/services/composeData.js
+++ frontend/resources/pages/pdp/services/composeData.js
@@ -12,10 +12,10 @@ const getData = async (productID) => {
     let result = R.clone(data);
 
     await getDataFromES(result, productID);
-
     result.relatedArticles = buildRelatedArticles(result.relatedArticles);
     result.productDetail = buildProductDetail(result.product, result.variant);
     result.productDetail.transformedImages = fallbackImage(result.productDetail);
+    result.preload = `<link rel="preload" as="image" href="${result.productDetail.transformedImages[0].image.small_1x1.url}">`
     result.variantGroups = buildVariantGroups(
         result.variant._source.url,
         result.variant._source.variantTypes,
